4549e6907823d1994fb1c40a02d71b71bb24f692
hxf-melo <748702350@qq.com>
2021-02-01 21:40:16 +0800

    初始化

---
+++ src/components/common/login/Login.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="login">
+    <div class="login-content">
+      <div class="login-avatar">
+        <img src="@/assets/img/logo/logo.png" alt="" />
+      </div>
+      <el-form
+        class="login-form"
+        ref="formReset"
+        :rules="loginFormRules"
+        :model="loginForm"
+        label-width="0px"
+      >
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="login-btn">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="loginFormReset">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      loginFormRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名:",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 8,
+            message: "长度在 3 到 8 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码:",
+            trigger: "blur",
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    //重置
+    loginFormReset() {
+      this.$refs.formReset.resetFields();
+    },
+    login() {
+      //登录预校验
+      this.$refs.formReset.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return this.$message.error("登录失败");
+        this.$message.success("登录成功");
+        console.log(res);
+        window.sessionStorage.setItem("token", res.data.token);
+        this.$router.push("/home");
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login-content {
+  width: 450px;
+  height: 350px;
+  border-radius: 10px;
+  background-color: #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .login-avatar {
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    border: 1px solid #eee;
+    box-shadow: 0 0 10px #ddd;
+    padding: 10px;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.login-btn {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Login = () =>
+    import ('@/components/common/login/Login.vue')
+
+const Home = () =>
+    import ('@/views/home/Home')
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
+    redirect: 'Login'
+}, {
+    path: '/login',
+    component: Login
+}, {
+    path: '/home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+}]
 
 const router = new VueRouter({
-  routes
+    routes,
+    mode: 'history',
 })
 
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next();
+    const sessionStorageStr = window.sessionStorage.getItem('token')
+    if (!sessionStorageStr) return next('/login')
+    next()
+})
 export default router
